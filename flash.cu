bc8a6c224192da01d70d1bc6776a54f99106fe18
Fan Xie <xiefan.fxie@bytedance.com>
2025-03-23 16:31:48 +0800

    modify

--- flash.cu
+++ flash.cu
@@ -223,6 +223,11 @@ __global__ void flash_forward(void* output, const void* q, const void* k,
   // ((2,2),MMA_M,MMA_K)
   auto rAccOut =
       partition_fragment_C(tiled_mma, Shape<Int<kBlockM>, Int<kHeadDim>>{});
+
+  if (thread0()) {
+    PRINT("rAccOut", rAccOut);
+  }
+
   auto scores_max =
       make_tensor<float>(Shape<Int<2 * size<1>(rAccOut)>>{});  // (2*MMA_M)
   auto scores_sum = make_fragment_like(scores_max);
