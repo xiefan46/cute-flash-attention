a2f1bc001132cb94f29de121166c7eb5bb9a9ba3
Fan Xie <xiefan.fxie@bytedance.com>
2025-03-26 17:22:50 +0800

    modify

--- flash_attn/flash_no_softmax.cu
+++ flash_attn/flash_no_softmax.cu
@@ -14,6 +14,89 @@ using namespace cute;
     print(content);          \
     print("\n");
 
+namespace config {
+using namespace cute;
+
+template <typename T_, int kHeadDim_ = 64, int kBlockM_ = 64, int kBlockN_ = 64>
+struct FlashConfig {
+  using T = T_;
+  static constexpr int kHeadDim = kHeadDim_;
+  static constexpr int kBlockM = kBlockM_;
+  static constexpr int kBlockN = kBlockN_;
+
+  static constexpr int kBlockKSmem = kHeadDim % 64 == 0 ? 64 : 32;
+  static constexpr int kBlockKGmem =
+      kHeadDim % 128 == 0 ? 128 : (kHeadDim % 64 == 0 ? 64 : 32);
+  static constexpr int kSwizzle = kBlockKSmem == 32 ? 2 : 3;
+  using SmemLayoutAtom = decltype(composition(
+      Swizzle<kSwizzle, 3, 3>{}, Layout<Shape<Int<8>, Int<kBlockKSmem>>,
+                                        Stride<Int<kBlockKSmem>, Int<1>>>{}));
+  using SmemLayoutQ = decltype(tile_to_shape(
+      SmemLayoutAtom{}, Shape<Int<kBlockM>, Int<kHeadDim>>{}));
+  using SmemLayoutKV = decltype(tile_to_shape(
+      SmemLayoutAtom{}, Shape<Int<kBlockN>, Int<kHeadDim>>{}));
+
+  using SmemLayoutAtomVtransposedNoSwizzle =
+      Layout<Shape<Int<kBlockKSmem>, Int<kBlockN>>,
+             Stride<Int<1>, Int<kBlockKSmem>>>;
+  using SmemLayoutAtomVtransposed = decltype(composition(
+      Swizzle<kSwizzle, 3, 3>{}, SmemLayoutAtomVtransposedNoSwizzle{}));
+  using SmemLayoutVtransposed = decltype(tile_to_shape(
+      SmemLayoutAtomVtransposed{}, Shape<Int<kHeadDim>, Int<kBlockN>>{}));
+  using SmemLayoutVtransposedNoSwizzle =
+      decltype(tile_to_shape(SmemLayoutAtomVtransposedNoSwizzle{},
+                             Shape<Int<kHeadDim>, Int<kBlockN>>{}));
+
+  using SmemCopyAtom = Copy_Atom<SM75_U32x4_LDSM_N, T>;
+  using SmemCopyAtomTransposed = Copy_Atom<SM75_U16x8_LDSM_T, T>;
+  using SmemLayoutAtomO = decltype(composition(
+      Swizzle<kSwizzle, 3, 3>{}, Layout<Shape<Int<8>, Int<kBlockKSmem>>,
+                                        Stride<Int<kBlockKSmem>, Int<1>>>{}));
+  using SmemLayoutO = decltype(tile_to_shape(
+      SmemLayoutAtomO{}, Shape<Int<kBlockM>, Int<kHeadDim>>{}));
+  using SmemCopyAtomO = Copy_Atom<DefaultCopy, T>;
+
+  using mma_op = SM80_16x8x16_F32F16F16F32_TN;
+  using mma_traits = MMA_Traits<mma_op>;
+  using mma_atom = MMA_Atom<mma_traits>;
+  static constexpr int kMmaEURepeatM = 4;
+  static constexpr int kMmaEURepeatN = 1;
+  static constexpr int kMmaEURepeatK = 1;
+
+  using mma_atom_shape = mma_traits::Shape_MNK;
+  static constexpr int kMmaPM = 1 * kMmaEURepeatM * get<0>(mma_atom_shape{});
+  static constexpr int kMmaPN = 2 * kMmaEURepeatN * get<1>(mma_atom_shape{});
+  static constexpr int kMmaPK = 1 * kMmaEURepeatK * get<2>(mma_atom_shape{});
+
+  using MMA_EU_RepeatT = decltype(make_layout(make_shape(
+      Int<kMmaEURepeatM>{}, Int<kMmaEURepeatN>{}, Int<kMmaEURepeatK>{})));
+  using MMA_P_T = Tile<Int<kMmaPM>, Int<kMmaPN>, Int<kMmaPK>>;
+
+  using TiledMMA =
+      decltype(make_tiled_mma(mma_atom{}, MMA_EU_RepeatT{}, MMA_P_T{}));
+  static constexpr int kThreadNum = size(TiledMMA{});
+  using g2s_copy_op = SM80_CP_ASYNC_CACHEGLOBAL<cute::uint128_t>;
+  using g2s_copy_traits = Copy_Traits<g2s_copy_op>;
+  static constexpr int kGmemThreadsPerRow = kBlockKSmem / 8;
+  using gmem_copy_atom = Copy_Atom<g2s_copy_traits, cute::half_t>;
+  using gmem_thr_layout = Layout<
+      Shape<Int<kThreadNum / kGmemThreadsPerRow>, Int<kGmemThreadsPerRow>>,
+      Stride<Int<kGmemThreadsPerRow>, Int<1>>>;
+  using gmem_val_layout = Layout<Shape<Int<1>, Int<8>>>;
+  using GmemTiledCopyQKV = decltype(make_tiled_copy(
+      gmem_copy_atom{}, gmem_thr_layout{}, gmem_val_layout{}));
+  using s2g_copy_atom = Copy_Atom<UniversalCopy<cute::uint128_t>, T>;
+  using GmemTiledCopyO = decltype(make_tiled_copy(
+      s2g_copy_atom{}, gmem_thr_layout{}, gmem_val_layout{}));
+
+  static constexpr int shm_size_q = cute::cosize(SmemLayoutQ{});
+  static constexpr int shm_size_kv = cute::cosize(SmemLayoutKV{}) * 2;
+  static constexpr int kShmSize = (shm_size_kv + shm_size_q) * sizeof(half);
+};
+
+}  // namespace config
+
+
 template <typename config>
 __global__ void flash_forward(void* output, const void* q, const void* k,
                               const void* v, int head_stride, int q_len,
@@ -366,87 +449,7 @@ __global__ void flash_forward(void* output, const void* q, const void* k,
   cute::copy(gmem_tiled_copy_O, tOsO, tOgO);
 }
 
-namespace config {
-using namespace cute;
 
-template <typename T_, int kHeadDim_ = 64, int kBlockM_ = 64, int kBlockN_ = 64>
-struct FlashConfig {
-  using T = T_;
-  static constexpr int kHeadDim = kHeadDim_;
-  static constexpr int kBlockM = kBlockM_;
-  static constexpr int kBlockN = kBlockN_;
-
-  static constexpr int kBlockKSmem = kHeadDim % 64 == 0 ? 64 : 32;
-  static constexpr int kBlockKGmem =
-      kHeadDim % 128 == 0 ? 128 : (kHeadDim % 64 == 0 ? 64 : 32);
-  static constexpr int kSwizzle = kBlockKSmem == 32 ? 2 : 3;
-  using SmemLayoutAtom = decltype(composition(
-      Swizzle<kSwizzle, 3, 3>{}, Layout<Shape<Int<8>, Int<kBlockKSmem>>,
-                                        Stride<Int<kBlockKSmem>, Int<1>>>{}));
-  using SmemLayoutQ = decltype(tile_to_shape(
-      SmemLayoutAtom{}, Shape<Int<kBlockM>, Int<kHeadDim>>{}));
-  using SmemLayoutKV = decltype(tile_to_shape(
-      SmemLayoutAtom{}, Shape<Int<kBlockN>, Int<kHeadDim>>{}));
-
-  using SmemLayoutAtomVtransposedNoSwizzle =
-      Layout<Shape<Int<kBlockKSmem>, Int<kBlockN>>,
-             Stride<Int<1>, Int<kBlockKSmem>>>;
-  using SmemLayoutAtomVtransposed = decltype(composition(
-      Swizzle<kSwizzle, 3, 3>{}, SmemLayoutAtomVtransposedNoSwizzle{}));
-  using SmemLayoutVtransposed = decltype(tile_to_shape(
-      SmemLayoutAtomVtransposed{}, Shape<Int<kHeadDim>, Int<kBlockN>>{}));
-  using SmemLayoutVtransposedNoSwizzle =
-      decltype(tile_to_shape(SmemLayoutAtomVtransposedNoSwizzle{},
-                             Shape<Int<kHeadDim>, Int<kBlockN>>{}));
-
-  using SmemCopyAtom = Copy_Atom<SM75_U32x4_LDSM_N, T>;
-  using SmemCopyAtomTransposed = Copy_Atom<SM75_U16x8_LDSM_T, T>;
-  using SmemLayoutAtomO = decltype(composition(
-      Swizzle<kSwizzle, 3, 3>{}, Layout<Shape<Int<8>, Int<kBlockKSmem>>,
-                                        Stride<Int<kBlockKSmem>, Int<1>>>{}));
-  using SmemLayoutO = decltype(tile_to_shape(
-      SmemLayoutAtomO{}, Shape<Int<kBlockM>, Int<kHeadDim>>{}));
-  using SmemCopyAtomO = Copy_Atom<DefaultCopy, T>;
-
-  using mma_op = SM80_16x8x16_F32F16F16F32_TN;
-  using mma_traits = MMA_Traits<mma_op>;
-  using mma_atom = MMA_Atom<mma_traits>;
-  static constexpr int kMmaEURepeatM = 4;
-  static constexpr int kMmaEURepeatN = 1;
-  static constexpr int kMmaEURepeatK = 1;
-
-  using mma_atom_shape = mma_traits::Shape_MNK;
-  static constexpr int kMmaPM = 1 * kMmaEURepeatM * get<0>(mma_atom_shape{});
-  static constexpr int kMmaPN = 2 * kMmaEURepeatN * get<1>(mma_atom_shape{});
-  static constexpr int kMmaPK = 1 * kMmaEURepeatK * get<2>(mma_atom_shape{});
-
-  using MMA_EU_RepeatT = decltype(make_layout(make_shape(
-      Int<kMmaEURepeatM>{}, Int<kMmaEURepeatN>{}, Int<kMmaEURepeatK>{})));
-  using MMA_P_T = Tile<Int<kMmaPM>, Int<kMmaPN>, Int<kMmaPK>>;
-
-  using TiledMMA =
-      decltype(make_tiled_mma(mma_atom{}, MMA_EU_RepeatT{}, MMA_P_T{}));
-  static constexpr int kThreadNum = size(TiledMMA{});
-  using g2s_copy_op = SM80_CP_ASYNC_CACHEGLOBAL<cute::uint128_t>;
-  using g2s_copy_traits = Copy_Traits<g2s_copy_op>;
-  static constexpr int kGmemThreadsPerRow = kBlockKSmem / 8;
-  using gmem_copy_atom = Copy_Atom<g2s_copy_traits, cute::half_t>;
-  using gmem_thr_layout = Layout<
-      Shape<Int<kThreadNum / kGmemThreadsPerRow>, Int<kGmemThreadsPerRow>>,
-      Stride<Int<kGmemThreadsPerRow>, Int<1>>>;
-  using gmem_val_layout = Layout<Shape<Int<1>, Int<8>>>;
-  using GmemTiledCopyQKV = decltype(make_tiled_copy(
-      gmem_copy_atom{}, gmem_thr_layout{}, gmem_val_layout{}));
-  using s2g_copy_atom = Copy_Atom<UniversalCopy<cute::uint128_t>, T>;
-  using GmemTiledCopyO = decltype(make_tiled_copy(
-      s2g_copy_atom{}, gmem_thr_layout{}, gmem_val_layout{}));
-
-  static constexpr int shm_size_q = cute::cosize(SmemLayoutQ{});
-  static constexpr int shm_size_kv = cute::cosize(SmemLayoutKV{}) * 2;
-  static constexpr int kShmSize = (shm_size_kv + shm_size_q) * sizeof(half);
-};
-
-}  // namespace config
 
 
 torch::Tensor forward_no_softmax(torch::Tensor q, torch::Tensor k, torch::Tensor v) {
@@ -469,7 +472,7 @@ torch::Tensor forward_no_softmax(torch::Tensor q, torch::Tensor k, torch::Tensor
   config::FlashConfig<cute::half_t> config;
 
   int bx = (q_len + config.kBlockM - 1) / config.kBlockM;
-  std::cout<<"q len: "<<q_len<<" bx:"<<bx<<std::endl;
+  std::cout<<"q len: "<<q_len<<", bx="<<bx<<", config.kBlockM="<<config.kBlockM<<std::endl;
 
   dim3 block = config.kThreadNum;
   dim3 grid((q_len + config.kBlockM - 1) / config.kBlockM, bs * head_num);
