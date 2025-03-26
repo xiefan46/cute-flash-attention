b1cc904a1fb399cba2a6b2e498bed516a9331869
Fan Xie <xiefan.fxie@bytedance.com>
2025-03-26 09:26:00 +0800

    modify

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 torch::Tensor forward(torch::Tensor q, torch::Tensor k, torch::Tensor v);
 torch::Tensor forward_no_softmax(torch::Tensor q, torch::Tensor k, torch::Tensor v);
+torch::Tensor forward_no_softmax_no_normal(torch::Tensor q, torch::Tensor k, torch::Tensor v);
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   // m.def("forward", torch::wrap_pybind_function(forward), "forward");
