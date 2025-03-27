4f43fad04f5c15ff5c9304c8738b6c4fb362a02c
Fan Xie <xiefan.fxie@bytedance.com>
2025-03-27 21:59:19 +0800

    modify

--- lightning_attn_cute/light_attn_without_decay.cu
+++ lightning_attn_cute/light_attn_without_decay.cu
@@ -145,7 +145,7 @@ __global__ void flash_forward(const half_t* q, const half_t* k, const half_t* v,
     if (thread0()) {
       // PRINT_TENSOR("tCrS after partition fragment", tCrS);
       // PRINT_TENSOR("tArS after partition fragment", tArS);
-      PRINT_TENSOR("tCrS", tCrS);
+      // PRINT_TENSOR("tCrS", tCrS);
       PRINT_TENSOR("tArS", tArS);
     }
 
