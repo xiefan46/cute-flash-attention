9bc57b5b5ac74cd0f0b167a1e4baf3def8920af8
Fan Xie <xiefan.fxie@bytedance.com>
2025-03-27 17:52:52 +0800

    modify

--- lightning_attn_cute/light_attn_without_decay.cu
+++ lightning_attn_cute/light_attn_without_decay.cu
@@ -158,6 +158,6 @@ torch::Tensor forward_without_decay(torch::Tensor q, torch::Tensor k, torch::Ten
 
   partition_kernel<<<grid, block, shm_size>>>(
       (const void*)q.data_ptr(),
-      (const void*)k.data_ptr(), (const void*)v.data_ptr(), (void*)out.data_ptr(), (const void*)slope.data_ptr(), B, H, N, d);
+      (const void*)k.data_ptr(), (const void*)v.data_ptr(), (void*)out.data_ptr(), B, H, N);
   return out;
 }
