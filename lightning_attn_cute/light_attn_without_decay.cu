0a7882afd7ffd716ed59c29626a57f498213048c
Fan Xie <fxie46@gmail.com>
2025-03-31 18:27:53 +0800

    modify

--- lightning_attn_cute/light_attn_without_decay.cu
+++ lightning_attn_cute/light_attn_without_decay.cu
@@ -193,7 +193,7 @@ __global__ void flash_forward(const half_t* q, const half_t* k, const half_t* v,
     }
 
     Tensor tOrO_intra = partition_fragment_C(mma, make_shape(Int<BLOCK>{}, Int<kHeadDim>{})); //BLOCK x d
-    cute::clear(tOrO_intra);
+//    cute::clear(tOrO_intra);
     cute::gemm(tOrS, tOrVt, tOrO_intra);
 //
 //    if (thread0()) {
