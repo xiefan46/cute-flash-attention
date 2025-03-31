2e330ccfe38aa8569d2c681c7de3f5803b696f1d
Fan Xie <fxie46@gmail.com>
2025-03-31 18:44:43 +0800

    modify

--- lightning_attn_cute/light_attn_without_decay.cu
+++ lightning_attn_cute/light_attn_without_decay.cu
@@ -211,7 +211,7 @@ __global__ void flash_forward(const half_t* q, const half_t* k, const half_t* v,
       PRINT("tOrO_intra size", size(tOrO_intra));
     }
 
-    // cute::gemm(tOrS, tOrVt, tOrO_intra);
+    cute::gemm(mma, tOrS, tOrVt, tOrO_intra);
 //
 //    if (thread0()) {
 //      PRINT_TENSOR("tOrO_intra", tOrO_intra);
