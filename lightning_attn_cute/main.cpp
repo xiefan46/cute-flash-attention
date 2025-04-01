315cbec486a182e66387ab7b10c31635090257de
Fan Xie <fxie46@gmail.com>
2025-04-01 17:44:48 +0800

    modify

--- lightning_attn_cute/main.cpp
+++ lightning_attn_cute/main.cpp
@@ -1,7 +1,8 @@
 #include <torch/extension.h>
+#include <tuple>
 
 torch::Tensor forward_without_decay(torch::Tensor q, torch::Tensor k, torch::Tensor v);
-torch::Tensor forward_without_decay_precision(torch::Tensor q, torch::Tensor k, torch::Tensor v);
+std::tuple<torch::Tensor, torch::Tensor> forward_without_decay_precision(torch::Tensor q, torch::Tensor k, torch::Tensor v);
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   m.def("forward_without_decay", torch::wrap_pybind_function(forward_without_decay), "forward_without_decay");
