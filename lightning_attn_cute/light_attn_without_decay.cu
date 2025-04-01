9e939fa7c31c21860abc98d8853a4d14d09572f0
Fan Xie <fxie46@gmail.com>
2025-04-01 12:53:45 +0800

    modify

--- lightning_attn_cute/light_attn_without_decay.cu
+++ lightning_attn_cute/light_attn_without_decay.cu
@@ -185,10 +185,6 @@ __global__ void flash_forward(const half_t* q, const half_t* k, const half_t* v,
     Tensor tBrKVt = thr_mma.partition_fragment_B(sKVt);
     cute::copy(tBsKVt, tBrKVt);
 
-    if (thread0()) {
-      PRINT_TENSOR("tBrKVt", tBrKVt)
-    }
-
     cute::gemm(mma, tArQ, tBrKVt, tCrO_inter);
 
 //    if (thread0()) {
