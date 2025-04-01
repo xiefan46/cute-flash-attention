fa814f63ca7f2cc12964c97a81ac39535c55afb1
Fan Xie <fxie46@gmail.com>
2025-04-01 11:48:10 +0800

    modify

--- lightning_attn_cute/test.py
+++ lightning_attn_cute/test.py
@@ -88,7 +88,7 @@ def lightning_attn_no_decay(
         )
         qkv_diag = torch.matmul(qk, vi.to(torch.float32))
         output[:, :, si:ei] = qkv_none_diag + qkv_diag
-        print(f"output: {output[0, 0, 0:2, 0: 2]}")
+        print(f"output: {output[0, 0, si:si+2, 0: 2]}")
         new_kv = torch.matmul(ki.transpose(-1, -2).to(vi.dtype), vi).to(torch.float32)
         print(f"torch new_kv: {new_kv[0, 0, 0: 2, 0: 2]}")
         kv = kv + new_kv
