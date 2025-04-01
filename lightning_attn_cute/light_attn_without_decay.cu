0e42cab7b94e8f6e7d7c5d201c9836712686ed59
Fan Xie <fxie46@gmail.com>
2025-04-01 12:57:40 +0800

    modify

--- lightning_attn_cute/light_attn_without_decay.cu
+++ lightning_attn_cute/light_attn_without_decay.cu
@@ -109,10 +109,10 @@ __global__ void flash_forward(const half_t* q, const half_t* k, const half_t* v,
   const int num_block = (N + BLOCK - 1) / BLOCK;
 
   __shared__ half_t smem_kv[kHeadDim * kHeadDim];
-  for (int i = tx; i < kHeadDim * kHeadDim; i += blockDim.x) {
-    smem_kv[i] = __float2half(0.0f);
-  }
-  __syncthreads();
+//  for (int i = tx; i < kHeadDim * kHeadDim; i += blockDim.x) {
+//    smem_kv[i] = __float2half(0.0f);
+//  }
+//  __syncthreads();
 
 
 
