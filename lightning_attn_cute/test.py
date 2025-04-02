301416fc63ad3f1f93f0bd0f0c3caca4f37200c4
Fan Xie <fxie46@gmail.com>
2025-04-02 18:20:44 +0800

    modify

--- lightning_attn_cute/test.py
+++ lightning_attn_cute/test.py
@@ -275,7 +275,9 @@ def test_kv_match(k, v, myflash):
 
         torch.testing.assert_close(
             torch_kv_output[i],
-            cute_kv_output[i]
+            cute_kv_output[i],
+            rtol=1e-2,
+            atol=1e-4,
         )
 
         print(f"✅ block : {i}, kv results match")
