4259f30c25158229344082fa065a69fea185f44e
Fan Xie <fxie46@gmail.com>
2025-04-02 19:23:47 +0800

    modify

--- lightning_attn_cute/light_attn_without_decay_precision_all_f16.cu
+++ lightning_attn_cute/light_attn_without_decay_precision_all_f16.cu
@@ -118,7 +118,7 @@ __global__ void compute_kv_kernel_all_f16(const half_t* k, const half_t* v, half
 
     __syncthreads();
 
-    cute::half_t one = cute::half_t(1.0f);
+    half_t one = half_t(1.0f);
 
     cute::axpby(one, tCrNewKV, one, tCsKV);
 
