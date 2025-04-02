a0a6afb3c6f68b4d6decf78b09a1774a3679560b
Fan Xie <fxie46@gmail.com>
2025-04-02 19:10:35 +0800

    modify

--- lightning_attn_cute/test.py
+++ lightning_attn_cute/test.py
@@ -303,7 +303,8 @@ def test_kv_match(k, v, myflash):
 
 def test_kv_match_f16(k, v, myflash):
     torch_kv_output = torch_compute_kv_f16(k, v)
-    cute_kv_output = myflash.cute_compute_kv_all_f16(k, v)
+    # cute_kv_output = myflash.cute_compute_kv_all_f16(k, v)
+    cute_kv_output = myflash.cute_compute_kv(k, v)
 
     print(f"torch_kv_output dtype: {torch_kv_output.dtype}, cute_kv_output dtype: {cute_kv_output.dtype}")
 
