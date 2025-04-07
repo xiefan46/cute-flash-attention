be262a7dd8cb694542f388334cd34d40ef594f0c
Fan Xie <fxie46@gmail.com>
2025-04-07 17:18:01 +0800

    modify

--- lightning_attn_cute/light_attention.cu
+++ lightning_attn_cute/light_attention.cu
@@ -209,7 +209,7 @@ __global__ void flash_forward(const half_t* q, const half_t* k, const half_t* v,
 
     auto elementwise_add_op = [] (auto a, auto b) {
         return a + b;
-    }
+    };
 
     for (int block_id = 0; block_id < num_block; block_id++) {
         Tensor gQ = local_tile(Q, make_tile(Int<BLOCK>{}, Int<kHeadDim>{}), make_coord(block_id, 0)); //BLOCK x d
