1ab10f0886962ba3be8fc75088d29c9bcf19a9f0
Fan Xie <fxie46@gmail.com>
2025-04-07 21:06:20 +0800

    modify

--- lightning_attn_cute/light_attention.cu
+++ lightning_attn_cute/light_attention.cu
@@ -254,10 +254,10 @@ __global__ void flash_forward(const half_t* q, const half_t* k, const half_t* v,
 //          PRINT("tCrS_fp16_decay", tCrS_fp16_decay);
 //        }
 
-        cute::transform(diag_decay_r, tCrS_fp16, tCrS_fp16, multiply_op);
-        if (thread0()) {
-            PRINT_TENSOR("tCrS_fp16", tCrS_fp16)
-        }
+//        cute::transform(diag_decay_r, tCrS_fp16, tCrS_fp16, multiply_op);
+//        if (thread0()) {
+//            PRINT_TENSOR("tCrS_fp16", tCrS_fp16)
+//        }
 
         // Step 2: compute O_intra
         // 将tCrS_f16转换为A layout，并且进行第二个gemm的计算
