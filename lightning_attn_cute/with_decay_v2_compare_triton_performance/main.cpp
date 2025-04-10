6fa34d986e7fd4a74eb7c417c8a69a18d25943ef
Fan Xie <fxie46@gmail.com>
2025-04-10 11:02:25 +0800

    modify

--- lightning_attn_cute/with_decay_v2_compare_triton_performance/main.cpp
+++ lightning_attn_cute/with_decay_v2_compare_triton_performance/main.cpp
@@ -9,6 +9,10 @@
 torch::Tensor forward_without_decay(torch::Tensor q, torch::Tensor k, torch::Tensor v,
                                                        torch::Tensor q_decay, torch::Tensor k_decay, torch::Tensor diag_decay, torch::Tensor block_decay);
 
+torch::Tensor forward_with_decay(torch::Tensor q, torch::Tensor k, torch::Tensor v,
+                                                       torch::Tensor q_decay, torch::Tensor k_decay, torch::Tensor diag_decay, torch::Tensor block_decay);
+
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   m.def("forward_without_decay", torch::wrap_pybind_function(forward_without_decay), "forward_without_decay");
+  m.def("forward_with_decay", torch::wrap_pybind_function(forward_with_decay), "forward_with_decay");
 }
