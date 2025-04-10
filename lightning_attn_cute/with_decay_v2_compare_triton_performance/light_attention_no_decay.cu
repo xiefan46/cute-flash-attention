c1e23c3f0620ee1aa43807d98f34ac2aa6802f28
Fan Xie <fxie46@gmail.com>
2025-04-10 10:58:58 +0800

    modify

--- lightning_attn_cute/with_decay_v2_compare_triton_performance/light_attention_no_decay.cu
+++ lightning_attn_cute/with_decay_v2_compare_triton_performance/light_attention_no_decay.cu
@@ -109,7 +109,7 @@ __forceinline__ __device__ auto load_decay_tensor_kt(const half_t* data_ptr, Thr
     using namespace cute;
     constexpr int BLOCK = config::BLOCK;
     constexpr int kHeadDim = config::kHeadDim;
-    Tensor g_decay = make_tensor(make_gmem_ptr<half_t>(data_ptr + head_id * BLOCK * kHeadDim), make_shape(Int<kHeadDim>{}, Int<BLOCK>{}), make_stride(Int<1>{}, Int<BLOCK>{}));
+    Tensor g_decay = make_tensor(make_gmem_ptr<half_t>(data_ptr + head_id * BLOCK * kHeadDim), make_shape(Int<kHeadDim>{}, Int<BLOCK>{}), make_stride(Int<1>{}, Int<kHeadDim>{}));
     Tensor g_decay_partition = thr_mma.partition_A(g_decay);
     Tensor r_decay = thr_mma.partition_fragment_A(g_decay);
     copy(g_decay_partition, r_decay);
