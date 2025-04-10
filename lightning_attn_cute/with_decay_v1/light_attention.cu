c37ab4ec223d8a5ce0e4431aac04abc60594a3db
Fan Xie <fxie46@gmail.com>
2025-04-10 17:48:51 +0800

    modify

--- lightning_attn_cute/with_decay_v1/light_attention.cu
+++ lightning_attn_cute/with_decay_v1/light_attention.cu
@@ -96,10 +96,10 @@ __forceinline__ __device__ auto fp16_to_fp32(Tensor& src_fp16) {
   auto src_fp16x2 = recast<float2>(src_fp16);
   auto dest_fp32x2 = recast<half2>(dest_fp32);
 #pragma unroll
-  for (int si = 0; si < size(dest_fp16x2); si++) {
+  for (int si = 0; si < size(dest_fp32x2); si++) {
     dest_fp32x2(si) = __half22float2_rn(src_fp16x2(si));
   }
-  return dest_3216;
+  return dest_fp32;
 }
 
 
