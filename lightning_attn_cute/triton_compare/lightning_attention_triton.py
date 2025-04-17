ec46a1f9c24b33e6debf13d52af3faa024de51ff
Fan Xie <fxie46@gmail.com>
2025-04-17 16:33:01 +0800

    modify

--- lightning_attn_cute/triton_compare/lightning_attention_triton.py
+++ lightning_attn_cute/triton_compare/lightning_attention_triton.py
@@ -52,6 +52,7 @@ def fwd_kernel_v4(
     q_decay_off_debug = tl.arange(0, BLOCK)
     tl.static_print("q_decay_off_debug")
     tl.static_print(q_decay_off_debug[None, :])
+    tl.static_print(q_decay_off_debug)
 
     # q_decay_off = q_decay_out + bx * BLOCK + tl.arange(0, BLOCK)
     # tl.store(q_decay_off[None, :],  q_decay[None, :])
