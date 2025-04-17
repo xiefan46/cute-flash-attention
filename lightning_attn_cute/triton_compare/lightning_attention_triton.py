54f17c8957537e2a3cd6ba423605042220c0621b
Fan Xie <fxie46@gmail.com>
2025-04-17 15:03:55 +0800

    modify

--- lightning_attn_cute/triton_compare/lightning_attention_triton.py
+++ lightning_attn_cute/triton_compare/lightning_attention_triton.py
@@ -5,6 +5,75 @@ import math
 import torch
 import torch.nn.functional as F
 import triton
+import triton
+import triton.language as tl
+
+@triton.jit
+def fwd_kernel_v4(
+        Q,
+        K,
+        V,
+        O,
+        S,  # log lambda
+        b: tl.constexpr,
+        h: tl.constexpr,
+        n: tl.constexpr,
+        d: tl.constexpr,
+        e: tl.constexpr,
+        BLOCK: tl.constexpr,
+        NUM_BLOCK: tl.constexpr,
+        BLOCK_MODEL: tl.constexpr,
+):
+    bx = tl.program_id(0)
+    by = tl.program_id(1)
+
+    block_off = tl.arange(0, BLOCK)
+    qk_dim_off = tl.arange(0, d)
+    vo_dim_off = tl.arange(0, BLOCK_MODEL) + by * BLOCK_MODEL
+    k_row_off = tl.arange(0, d)
+    # decay
+    head_off = bx % h
+    slope = tl.load(S + head_off).to(tl.float32)
+    q_decay = tl.exp(-slope * block_off[:, None]).to(tl.float16)
+    k_decay = tl.exp(-slope * (BLOCK - block_off[None, :])).to(tl.float16)
+    block_decay = tl.exp(-slope * BLOCK)
+    index = block_off[:, None] - block_off[None, :]  # 相对位置 BLOCK x BLOCK
+    s_index = -slope * index  # BLOCK * BLOCK
+    s_index = tl.where(index >= 0, s_index, float("-inf"))
+    diag_decay = tl.exp(s_index).to(tl.float16)
+
+    kv = tl.zeros((d, BLOCK_MODEL), dtype=tl.float32)
+
+    Q_start = Q + bx * n * d + qk_dim_off[None, :]
+    K_start = K + bx * n * d + k_row_off[:, None]
+    V_start = V + bx * n * e + vo_dim_off[None, :]
+    O_start = O + bx * n * e + vo_dim_off[None, :]
+
+    for i in range(NUM_BLOCK):
+        q_off = block_off[:, None] * d
+        q = tl.load(Q_start + q_off, mask=block_off[:, None] < n, other=0.0).to(tl.float16)
+
+        k_off = block_off[None, :] * d
+        k_t = tl.load(K_start + k_off, mask=block_off[None, :] < n, other=0.0).to(tl.float16)
+
+        vo_off = block_off[:, None] * e
+        v = tl.load(V_start + vo_off, mask=block_off[:, None] < n, other=0.0).to(tl.float16)
+
+
+        qk = tl.dot(q, k_t).to(tl.float16)
+        o_intra = tl.dot((qk * diag_decay).to(tl.float16), v).to(tl.float16)
+
+        kv_f16 = kv.to(tl.float16)
+        qkv_inter = tl.dot(q, kv_f16).to(tl.float16)
+        o_inter = (qkv_inter * q_decay).to(tl.float16)
+        o = o_intra + o_inter
+
+        tl.store(O_start + vo_off, o.to(O.dtype.element_ty), mask=block_off[:, None] < n)
+
+        new_kv = tl.dot(k_t * k_decay, v).to(tl.float16)
+        kv = kv * block_decay + new_kv.to(tl.float32)
+
+        block_off += BLOCK
 
 def lightning_attn2(q, k, v, s, kernel_impl, BLOCK):
     q = q.contiguous()
@@ -32,6 +101,9 @@ def lightning_attn2(q, k, v, s, kernel_impl, BLOCK):
         v_padded = v
 
     o_padded = torch.empty((b, h, n, e_padded), dtype=q.dtype, device=q.device)
+
+    # output debug info
+    # q_decay_out = torch.empty((b, h, ))
 
     NUM_BLOCK = triton.cdiv(q.shape[2], BLOCK)
     # parallel over channel
@@ -79,6 +151,7 @@ def next_power_of_2(n):
 def lightning_attn_func(q, k, v, s, kernel_impl, BLOCK):
     b, h, n, d = q.shape
     e = v.shape[-1]
+    assert d == e
     assert is_support(d) and is_support(e)
 
     # pad v's feature dim to power of 2
--- lightning_attn_cute/triton_compare/test_lightning_decay.py
+++ lightning_attn_cute/triton_compare/test_lightning_decay.py
@@ -193,8 +193,8 @@ def test_forward_with_decay(q, k, v, myflash):
     cute_output, cute_kv_output, cute_o_inter_out, cute_o_intra_out, cute_q_decay_out, cute_kv_t_out = myflash.forward_with_decay(q, k, v, q_decay_cute, k_decay_cute, diag_decay_cute, block_decay_cute)
 
     for i in range(num_block):
-        print(f"torch_kv_output shape: {torch_kv_output[i].shape}")
-        print(f"cute_kv_output shape: {cute_kv_output[i].shape}")
+        # print(f"torch_kv_output shape: {torch_kv_output[i].shape}")
+        # print(f"cute_kv_output shape: {cute_kv_output[i].shape}")
         # torch.testing.assert_close(
         #     torch_kv_output[i],
         #     cute_kv_output[i],
@@ -203,10 +203,10 @@ def test_forward_with_decay(q, k, v, myflash):
     print("✅ kv results match")
 
     for i in range(num_block):
-        print(f"torch_o_intra_out shape: {torch_o_intra_out[i].shape}")
-        print(f"cute_o_intra_out shape: {cute_o_intra_out[i].shape}")
-        print(f"torch_o_intra_out dtype/device: {torch_o_intra_out[i].dtype} device: {torch_o_intra_out[i].device}")
-        print(f"cute_o_intra_out dtype/device: {cute_o_intra_out[i].dtype}, device: {cute_o_intra_out[i].device}")
+        # print(f"torch_o_intra_out shape: {torch_o_intra_out[i].shape}")
+        # print(f"cute_o_intra_out shape: {cute_o_intra_out[i].shape}")
+        # print(f"torch_o_intra_out dtype/device: {torch_o_intra_out[i].dtype} device: {torch_o_intra_out[i].device}")
+        # print(f"cute_o_intra_out dtype/device: {cute_o_intra_out[i].dtype}, device: {cute_o_intra_out[i].device}")
         # torch.testing.assert_close(
         #     torch_o_intra_out[i],
         #     cute_o_intra_out[i],
@@ -215,10 +215,10 @@ def test_forward_with_decay(q, k, v, myflash):
     print("✅ o intra result maches")
 
     for i in range(num_block):
-        print(f"torch_q_decay_out shape: {torch_q_decay_out[i].shape}")
-        print(f"cute_q_decay_out shape: {cute_q_decay_out[i].shape}")
-        print(f"torch_q_decay_out dtype: {torch_q_decay_out[i].dtype}")
-        print(f"cute_q_decay_out dtype: {cute_q_decay_out[i].dtype}")
+        # print(f"torch_q_decay_out shape: {torch_q_decay_out[i].shape}")
+        # print(f"cute_q_decay_out shape: {cute_q_decay_out[i].shape}")
+        # print(f"torch_q_decay_out dtype: {torch_q_decay_out[i].dtype}")
+        # print(f"cute_q_decay_out dtype: {cute_q_decay_out[i].dtype}")
         # torch.testing.assert_close(
         #     torch_q_decay_out[i],
         #     cute_q_decay_out[i],
@@ -227,10 +227,10 @@ def test_forward_with_decay(q, k, v, myflash):
     print("✅ q_decay_out  result matches")
 
     for i in range(num_block):
-        print(f"torch_kv_t_out  shape: {torch_kv_t_out[i].shape}")
-        print(f"cute_kv_t_out shape: {cute_kv_t_out[i].shape}")
-        print(f"torch_kv_t_out dtype: {torch_kv_t_out[i].dtype}")
-        print(f"cute_kv_t_out dtype: {cute_kv_t_out[i].dtype}")
+        # print(f"torch_kv_t_out  shape: {torch_kv_t_out[i].shape}")
+        # print(f"cute_kv_t_out shape: {cute_kv_t_out[i].shape}")
+        # print(f"torch_kv_t_out dtype: {torch_kv_t_out[i].dtype}")
+        # print(f"cute_kv_t_out dtype: {cute_kv_t_out[i].dtype}")
         # torch.testing.assert_close(
         #     torch_kv_t_out[i],
         #     cute_kv_t_out[i],
@@ -239,8 +239,8 @@ def test_forward_with_decay(q, k, v, myflash):
     print("✅ kv_t_out  result matches")
 
     for i in range(num_block):
-        print(f"torch_o_inter_out shape: {torch_o_inter_out[i].shape}")
-        print(f"cute_o_inter_out shape: {cute_o_inter_out[i].shape}")
+        # print(f"torch_o_inter_out shape: {torch_o_inter_out[i].shape}")
+        # print(f"cute_o_inter_out shape: {cute_o_inter_out[i].shape}")
         # torch.testing.assert_close(
         #     torch_o_inter_out[i],
         #     cute_o_inter_out[i],
