c0c7386570e244df6a8508abd516afb0ed68906d
Fan Xie <fxie46@gmail.com>
2025-04-17 17:53:51 +0800

    modify

--- lightning_attn_cute/triton_compare/test_lightning_decay.py
+++ lightning_attn_cute/triton_compare/test_lightning_decay.py
@@ -292,10 +292,10 @@ def test_forward_with_decay(q, k, v, myflash):
 
     print(f"triton_q_decay_out shape: {triton_q_decay_out[0].shape}, triton_k_decay_out: {triton_k_decay_out[0].shape}, triton_diag_decay_out shape: {triton_diag_decay_out[0].shape}, triton_block_decay_out shape: {triton_block_decay_out[0].shape}")
 
-    # torch.testing.assert_allclose(q_decay.reshape(BLOCK, ), triton_q_decay_out[0, 0])
-    # torch.testing.assert_allclose(k_decay.reshape(BLOCK, ), triton_k_decay_out[0, 0])
-    # torch.testing.assert_allclose(diag_decay.reshape(BLOCK, BLOCK), triton_diag_decay_out[0, 0])
-    # torch.testing.assert_allclose(block_decay.reshape(1, ), triton_block_decay_out[0, 0] )
+    torch.testing.assert_allclose(q_decay.squeeze(), triton_q_decay_out[0])
+    torch.testing.assert_allclose(k_decay.squeeze(), triton_k_decay_out[0])
+    torch.testing.assert_allclose(diag_decay.squeeze(), triton_diag_decay_out[0])
+    torch.testing.assert_allclose(block_decay.squeeze(), triton_block_decay_out[0])
 
 
     for i in range(num_block):
